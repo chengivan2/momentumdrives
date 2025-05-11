00e2b67ffe0b9dd4dd4c812979908b5431e0b2a4
chengivan <chengoleivan@gmail.com>
2025-05-11 19:19:00 +0300

    Modified the CTAOne's bg

--- src/app/rootcomponents/header/components/CTAOne.tsx
+++ src/app/rootcomponents/header/components/CTAOne.tsx
@@ -4,7 +4,7 @@ export default function CTAOne() {
   return (
     <Link
       href="/"
-      className="min-w-full lg:min-w-auto flex flex-row justify-center items-center px-4 py-2 rounded-full text-sm font-medium
+      className="bg-red-600 min-w-full lg:min-w-auto flex flex-row justify-center items-center px-4 py-2 rounded-full text-sm font-medium
                      transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100"
     >
       Make your own trip
