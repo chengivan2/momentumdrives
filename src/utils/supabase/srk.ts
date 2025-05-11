3d9d0e10c89ba466d3ecb898f8eab958c7e056d1
chengivan <chengoleivan@gmail.com>
2025-05-11 20:10:23 +0300

    Added the srk Supbase client handler

---
+++ src/utils/supabase/srk.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
+
+
+export const supabaseService = createClient(supabaseUrl, supabaseRoleKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
+  }
+})
